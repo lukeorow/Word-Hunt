8ff6c8a12d4a6cf532f7ca9cfbc1b2dc3de29468
lukeorow <lukeorow@gmail.com>
2024-11-13 01:20:23 -0500

    added requirements.txt
    live deploying the website on Render

--- python/get_words.py
+++ python/get_words.py
@@ -1,13 +1,15 @@
 import json
 from itertools import permutations, combinations, product
-    
+
 # generates a set of all possible words that use the 7 letters and contain the special letter
 # words must also be 4 letters or longer and can use repeat letters
+
+
 def generate_words(letters, special_letter):
     with open('python/words_dictionary.json') as f:
         data = json.load(f)
         valid_words = set(data.keys())
-    
+
     found_words = set()
     for length in range(4, len(letters) + 1):
         for combo in combinations(letters, length):
@@ -16,5 +18,4 @@ def generate_words(letters, special_letter):
                     word = ''.join(perm)
                     if word in valid_words and special_letter in word:
                         found_words.add(word)
-    return found_words # returns a set
-
+    return found_words  # returns a set
